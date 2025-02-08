682da148678f50ea190e404507fe9f58b5e0d3ff
holmen1 <holmen1@gmail.com>
2025-02-08 16:02:32 +0100

    Exercise 2-6

--- 2_Types_Operators_and_Expressions/bits/bitwise.c
+++ 2_Types_Operators_and_Expressions/bits/bitwise.c
@@ -7,15 +7,41 @@ void print_binary(int n) {
   printf("\n");
 }
 
+/* getbits:  get n bits from position p */
+unsigned getbits(unsigned x, int p, int n)
+{
+  return (x >> (p+1-n)) & ~(~0 << n);
+}
+
+/* setbits:  set n rightmost bits of y from position p */
+unsigned setbits(unsigned x, int p, int n, unsigned y)
+{
+  int mask = ~(~0 << n);
+  return (x & ~(mask << (p + 1 - n))) | ((y & mask) << (p + 1 - n));
+}
+
+/* setbits: explained */
+unsigned setbits_steps(unsigned x, int p, int n, unsigned y)
+{
+  /* n rightmost 1s */
+  unsigned mask = ~(~0 << n);
+  /* clear n bits from position p in x */
+  x = x & ~(mask << (p + 1 - n));
+  /* first n y bits shifted to p to fit x above */
+  y = (y & mask) << (p + 1 - n);
+  return x | y;
+}
+
 int main() {
   char a = 'a';
-  printf("Decimal %c = %d\n", a, a);
-  printf("Binary %c = ", a);
+  printf("Decimal '%c'\t=\t%d\n", a, a);
+  printf("Binary '%c'\t=\t", a);
   print_binary(a);
   printf("\n");
 
-  int n = 7;
-  printf("n = %d\n", n);
+  printf("\n- complement\n");
+  int n = 77;
+  printf("n\t\t=\t%d\n", n);
   printf("Binary n\t=\t");
   print_binary(n);
   printf("Binary ~n\t=\t");
@@ -23,18 +49,48 @@ int main() {
   printf("Binary ~n+1\t=\t");
   print_binary(~n+1);
   printf("Decimal ~n+1\t=\t%d\n", ~n+1);
+
+  printf("getbits(n,6,4)\t=\t");
+  print_binary(getbits(n,6,4));
+
+  /* setbits */
+  unsigned x = 0b10101010;
+  unsigned y = 0b11001100;
+  int p = 4;
+  n = 3;
+
+  printf("\n- setbits\n");
+  printf("Binary x\t=\t");
+  print_binary(x);
+  printf("Binary y\t=\t");
+  print_binary(y);
+  printf("p\t\t=\t%d\n", p);
+  printf("n\t\t=\t%d\n", n);
+  printf("setbits(x,p,n,y)=\t");
+  print_binary(setbits(x,p,n,y));
+
   return 0;
 }
 
 /*
 $ gcc -g -Wall -std=c90 bitwise.c 
 $ ./a.out 
-Decimal a = 97
-Binary a = 00000000000000000000000001100001
+Decimal 'a'     =       97
+Binary 'a'      =       00000000000000000000000001100001
 
-n = 7
-Binary n        =       00000000000000000000000000000111
-Binary ~n       =       11111111111111111111111111111000
-Binary ~n+1     =       11111111111111111111111111111001
-Decimal ~n+1    =       -7
+
+- complement
+n               =       77
+Binary n        =       00000000000000000000000001001101
+Binary ~n       =       11111111111111111111111110110010
+Binary ~n+1     =       11111111111111111111111110110011
+Decimal ~n+1    =       -77
+getbits(n,6,4)  =       00000000000000000000000000001001
+
+- setbits
+Binary x        =       00000000000000000000000010101010
+Binary y        =       00000000000000000000000011001100
+p               =       4
+n               =       3
+setbits(x,p,n,y)=       00000000000000000000000010110010
 */
