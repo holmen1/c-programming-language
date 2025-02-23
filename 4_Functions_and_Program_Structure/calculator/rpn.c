285f216249548552f78ffef134ec17e033277d29
holmen1 <holmen1@gmail.com>
2025-02-23 11:09:00 +0100

    add stack

--- 4_Functions_and_Program_Structure/calculator/rpn.c
+++ 4_Functions_and_Program_Structure/calculator/rpn.c
@@ -1,10 +1,59 @@
 //
 //
 //
-#include <stdio.h>
 #include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
 
-#define BUFSIZE 100
+#define MAXVAL 100 /* maximum depth of val stack */
+#define NUMBER '0'
+#define BUFSIZE 100 /* maximum buffer size */
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f) {
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void) {
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+
+int getch(void);
+void ungetch(int);
+
+/* getop: get next operator or numeric operand */
+int getop(char s[])
+{
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;
+    i = 0;
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
 
 
 char buf[BUFSIZE]; /* buffer for ungetch */
@@ -21,6 +70,28 @@ void ungetch(int c) /* push character back to input */
         printf("ungetch: too many characters\n");
     else
         buf[bufp++] = c;
+}
+
+/* --- ASSERTS --- */
+void test_push_pop() {
+    // Test pushing and popping a single value
+    push(1.0);
+    assert(pop() == 1.0);
+
+    // Test pushing and popping multiple values
+    push(2.0);
+    push(3.0);
+    assert(pop() == 3.0);
+    assert(pop() == 2.0);
+
+    // Test popping from an empty stack
+    assert(pop() == 0.0); // Should print "error: stack empty"
+
+    // Test pushing to a full stack
+    for (int i = 0; i < MAXVAL; i++) {
+        push(i);
+    }
+    push(MAXVAL); // Should print "error: stack full"
 }
 
 void test_getch_ungetch() {
@@ -44,6 +115,7 @@ void test_getch_ungetch() {
 }
 
 int main() {
+    test_push_pop();
     test_getch_ungetch();
     printf("All tests passed.\n");
     return 0;
