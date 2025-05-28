a5a8bdbdb55691a50d82b974c34c91a9cdd463e5
holmen1 <holmen1@gmail.com>
2025-05-28 21:39:31 +0200

    Exercise 5-1

--- 5_Pointers_and_Arrays/getint/getint.c
+++ 5_Pointers_and_Arrays/getint/getint.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include "getch.h"
 
+int isnan(int c)
+{
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-')
+        return 1;
+    return 0;
+}
 
 /* getint: get next integer from input into *pn */
 int getint(int* pn)
@@ -10,13 +16,17 @@ int getint(int* pn)
 
     while (isspace(c = getch()))
         ;
-    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+    if (isnan(c)) {
         ungetch(c); /* not a number */
         return 0;
     }
     sign = (c == '-') ? -1 : 1;
     if (c == '+' || c == '-')
         c = getch();
+    if (isnan(c)) {
+        ungetch(c);
+        return 0;
+    }
     for (*pn = 0; isdigit(c); c = getch())
         *pn = 10 * *pn + (c - '0');
     *pn *= sign;
