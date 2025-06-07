826a71120579e471edf19d2752fa72c59c4e1a98
holmen1 <holmen1@gmail.com>
2025-06-07 10:17:47 +0200

    format

--- 5_Pointers_and_Arrays/tail/main.c
+++ 5_Pointers_and_Arrays/tail/main.c
@@ -1,37 +1,36 @@
-#include <stdio.h>
 #include "readlines.h"
+#include <stdio.h>
 
 #define MAXLINES 5000
 #define DEFAULT_N 10
 
-char* lineptr[MAXLINES];
+char *lineptr[MAXLINES];
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   int nlines;
   int n_to_print = DEFAULT_N;
   char *filename = NULL;
 
   if (argc == 1) {
-      fprintf(stderr, "Usage: %s filename\n", argv[0]);
-      return 1;
+    fprintf(stderr, "Usage: %s filename\n", argv[0]);
+    return 1;
   } else if (argc == 2) {
-      filename = argv[1];
+    filename = argv[1];
   } else {
-      fprintf(stderr, "Usage: %s filename\n", argv[0]);
-      return 1;
+    fprintf(stderr, "Usage: %s filename\n", argv[0]);
+    return 1;
   }
 
   if (filename != NULL) {
-      if (freopen(filename, "r", stdin) == NULL) {
-          fprintf(stderr, "Error: Can't open file %s\n", filename);
-          return 1;
-      }
+    if (freopen(filename, "r", stdin) == NULL) {
+      fprintf(stderr, "Error: Can't open file %s\n", filename);
+      return 1;
+    }
   }
 
   if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
 
-    if (nlines <= n_to_print) 
+    if (nlines <= n_to_print)
       writelines(lineptr, nlines);
     else
       writelines(lineptr + nlines - n_to_print, n_to_print);
