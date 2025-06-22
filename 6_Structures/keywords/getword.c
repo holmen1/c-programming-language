41a9f9b0f2022708e16362f95a3307857f731590
holmen1 <holmen1@gmail.com>
2025-06-22 11:40:27 +0200

    Exercise 6-1

--- 6_Structures/keywords/getword.c
+++ 6_Structures/keywords/getword.c
@@ -7,28 +7,73 @@ void ungetch(int c);
 /* getword: get next word or character from input */
 int getword(char *word, int lim)
 {
-    int c, getch(void);
-    void ungetch(int);
+    int c, next_char;
     char *w = word;
 
     while (isspace(c = getch()))
         ;
-    if (c != EOF)
-        *w++ = c;
-    if (!isalpha(c)) {
-        *w = '\0';
-        return c;
-    }
-    for ( ; --lim > 0; w++)
-        if (!isalnum(*w = getch())) {
-            ungetch(*w);
-            break;
+    if (c == EOF)
+        return EOF;
+
+    *w++ = c;
+
+    /* Handle string constants */
+    if (c == '"') {
+        for (; --lim > 0; w++) {
+            *w = getch();
+            if (*w == '\\') { /* Handle escaped characters */
+                *++w = getch();
+            } else if (*w == '"' || *w == EOF) {
+                break;
+            }
         }
+        *w = '\0';
+        return word[0];
+    }
+
+    /* Handle multi-line comments */
+    if (c == '/') {
+        if ((next_char = getch()) == '*') { /* Multi-line comment start */
+            while ((c = getch()) != EOF) {
+                if (c == '*') {
+                    if ((next_char = getch()) == '/') {
+                        break; /* End of comment */
+                    } else {
+                        ungetch(next_char);
+                    }
+                }
+            }
+            return getword(word, lim); /* Recursively get the next token */
+        } else {
+            ungetch(next_char); /* Not a comment, just a slash */
+        }
+    }
+
+    /* Handle preprocessor directives */
+    if (c == '#') {
+        while ((c = getch()) != '\n' && c != EOF)
+            ; /* Skip to end of line */
+        return getword(word, lim); /* Get next token after the directive */
+    }
+
+    /* Handle identifiers (words) */
+    if (isalpha(c) || c == '_') {
+        for (; --lim > 0; w++) {
+            if (!isalnum(*w = getch()) && *w != '_') {
+                ungetch(*w);
+                break;
+            }
+        }
+        *w = '\0';
+        return word[0];
+    }
+
+    /* Handle single-character tokens (operators, etc.) */
     *w = '\0';
-    return word[0];
+    return c;
 }
 
-#define BUFSIZE 1 /* maximum buffer size */
+#define BUFSIZE 100 /* maximum buffer size */
 
 char buf[BUFSIZE]; /* buffer for ungetch */
 int bufp = 0;
--- 6_Structures/keywords/main.c
+++ 6_Structures/keywords/main.c
@@ -7,18 +7,21 @@ struct key {
     char *word;
     int count;
 } keytab[] = {
-    "auto", 0,
-    "break", 0,
-    "case", 0,
-    "char", 0,
-    "const", 0,
-    "continue", 0,
-    "default", 0,
-    "for", 0,
-    "if", 0,
-    "unsigned", 0,
-    "void", 0,
-    "while", 0
+    {"auto", 0},
+    {"break", 0},
+    {"case", 0},
+    {"char", 0},
+    {"const", 0},
+    {"continue", 0},
+    {"default", 0},
+    {"define", 0},
+    {"for", 0},
+    {"if", 0},
+    {"include", 0},
+    {"int", 0},
+    {"unsigned", 0},
+    {"void", 0},
+    {"while", 0}
 };
 
 #define NKEYS (sizeof keytab / sizeof(struct key))
