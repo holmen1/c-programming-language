aa732cdbaad07ef5adbb9f5910e5e188030535bd
holmen1 <holmen1@gmail.com>
2025-07-05 11:43:31 +0200

    ctd

--- LowLevelAcademy/network/network/src/srv/main.c
+++ LowLevelAcademy/network/network/src/srv/main.c
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <netinet/in.h>
+#include <getopt.h>
 
 #include "common.h"
 #include "file.h"
@@ -43,12 +44,13 @@ void poll_loop(unsigned short port, struct dbheader_t *dbhdr, struct employee_t 
     int opt = 1;  /* Option for setsockopt */
 
     /* Initialize client states */
-    init_clients(&client_states);
+    init_clients((clientstate_t *)&clientStates);
 
     /* Create server socket */
     if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket");
         exit(EXIT_FAILURE);
+    }
 
     if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
         perror("setsockopt");
@@ -93,8 +95,8 @@ void poll_loop(unsigned short port, struct dbheader_t *dbhdr, struct employee_t 
             }   
         }
 
-        // Wait for an event on one of the sockets
-        int n_events = poll(fds, nfds, -1); // -1 means no timeout
+        /* Wait for an event on one of the sockets */
+        int n_events = poll(fds, nfds, -1); /* -1 means no timeout */
         if (n_events == -1) {
             perror("poll");
             exit(EXIT_FAILURE);
@@ -110,13 +112,13 @@ void poll_loop(unsigned short port, struct dbheader_t *dbhdr, struct employee_t 
             printf("New connection from %s:%d\n",
             	inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-            freeSlot = find_free_slot(&clientStates);
+            freeSlot = find_free_slot((clientstate_t *)&clientStates);
             if (freeSlot == -1) {
                 printf("Server full: closing new connection\n");
                 close(conn_fd);
             } else {
                 clientStates[freeSlot].fd = conn_fd;
-                clientStates[freeSlot].state = STATE_HELLO;
+                clientStates[freeSlot].state = STATE_CONNECTED;
                 nfds++;
                 printf("Slot %d has fd %d\n", freeSlot, clientStates[freeSlot].fd);
             }
@@ -130,7 +132,7 @@ void poll_loop(unsigned short port, struct dbheader_t *dbhdr, struct employee_t 
                 n_events--;
 
                 int fd = fds[i].fd;
-                int slot = find_slot_by_fd(&clientStates, fd);
+                int slot = find_slot_by_fd((clientstate_t *)&clientStates, fd);
                 ssize_t bytes_read = read(fd, &clientStates[slot].buffer, sizeof(clientStates[slot].buffer));
                 if (bytes_read <= 0) {
                     // Connection closed or error
@@ -143,177 +145,93 @@ void poll_loop(unsigned short port, struct dbheader_t *dbhdr, struct employee_t 
                         nfds--;
                     }
                 } else {
-                    handle_client_fsm(dbhdr, employees, &clientStates[slot]);
+                    printf("TODO\n");
                 }
             }
         }
     }
-
 }
 
+int main(int argc, char *argv[]) { 
+	char *filepath = NULL;
+	char *portarg = NULL;
+	unsigned short port = 0;
+	bool newfile = false;
+	bool list = false;
+	int c;
 
-int main(void) {
-    int server_fd, client_fds[MAX_CLIENTS];
-    struct pollfd fds[MAX_CLIENTS + 1];  /* +1 for server socket */
-    int nfds = 1;  /* Number of file descriptors to monitor (start with server) */
-    int activity, i, new_socket, bytes_read;
-    char buffer[BUFFER_SIZE];
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_len = sizeof(client_addr);
-    
-    /* Initialize client array with -1 (no connection) */
-    for (i = 0; i < MAX_CLIENTS; i++) {
-        client_fds[i] = -1;
-    }
-    
-    /* Create server socket */
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-    
-    /* Set socket options to allow reuse of address/port
-       Prevents "Address already in use" errors */
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    
-    /* Prepare server address structure */
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-    
-    /* Bind socket to port */
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("bind");
-        exit(EXIT_FAILURE);
-    }
-    
-    /* Listen for incoming connections */
-    if (listen(server_fd, BACKLOG) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    
-    printf("Server started on port %d. Connect with: nc localhost %d\n", PORT, PORT);
-    
-    /* Initialize the pollfd array with server socket */
-    fds[0].fd = server_fd;
-    fds[0].events = POLLIN;  /* Interested in read events */
+	int dbfd = -1;
+	struct dbheader_t *dbhdr = NULL;
+	struct employee_t *employees = NULL;
 
-    /* Initialize remaining slots as unused */
-    for (i = 1; i <= MAX_CLIENTS; i++) {
-        fds[i].fd = -1;
-    }
-    
-    while (1) {
-        
-        printf("Waiting for activity...\n");
-        activity = poll(fds, nfds, -1);  /* -1 means wait indefinitely */
-        
-        if (activity < 0) {
-            perror("select");
-            break;
-        }
-        
-        /* Check if there's activity on the server socket (new connection) */
-        if (fds[0].revents & POLLIN) {
-            if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
-                perror("accept");
-                continue;
-            }
-            
-            printf("New connection from %s:%d on socket fd %d\n", 
-                   inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), new_socket);
-            
-            /* Add new client to an empty slot */
-            int added = 0;
-            for (i = 0; i < MAX_CLIENTS; i++) {
-                if (client_fds[i] == -1) {
-                    client_fds[i] = new_socket;
-                    added = 1;
-                    break;
-                }
-            }
-            
-            if (!added) {
-                printf("Too many clients, connection rejected\n");
-                close(new_socket);
-            } else {
-                /* Add to poll set */
-                fds[i+1].fd = new_socket;
-                fds[i+1].events = POLLIN;
-                if (i+1 >= nfds) {
-                    nfds = i+2;  /* Update the number of fds to monitor */
-                }
-                
-                /* Welcome message */
-                char welcome[] = "Welcome to the echo server! Type something and press enter.\n";
-                send(new_socket, welcome, strlen(welcome), 0);
-            }
-        }
-        
-        /* Check for activity on client sockets */
-        for (i = 1; i < nfds; i++) {
-            if (fds[i].fd != -1 && (fds[i].revents & POLLIN)) {
-                /* Data available from client */
-                bytes_read = read(fds[i].fd, buffer, BUFFER_SIZE - 1);
-                
-                if (bytes_read <= 0) {
-                    /* Connection closed or error */
-                    if (bytes_read == 0) {
-                        printf("Client on fd %d disconnected\n", fds[i].fd);
-                    } else {
-                        perror("read");
-                    }
-                    
-                    /* Close socket and remove from poll set */
-                    close(fds[i].fd);
-                    
-                    /* Find and clear the corresponding client_fds entry */
-                    int j = 0;
-                    for (; j < MAX_CLIENTS; j++) {
-                        if (client_fds[j] == fds[i].fd) {
-                            client_fds[j] = -1;
-                            break;
-                        }
-                    }
-                    
-                    /* Mark the pollfd slot as unused */
-                    fds[i].fd = -1;
-                    
-                    /* Compact the array if this was the last entry */
-                    if (i == nfds - 1) {
-                        nfds--;
-                        /* Find new last valid entry */
-                        while (nfds > 1 && fds[nfds-1].fd == -1) {
-                            nfds--;
-                        }
-                    }
-                } else {
-                    /* Echo back the message */
-                    buffer[bytes_read] = '\0';
-                    printf("Received from fd %d: %s", fds[i].fd, buffer);
-                    
-                    /* Add a prefix to show it's coming back from the server */
-                    char response[BUFFER_SIZE + 32];
-                    sprintf(response, "Server echoes: %s", buffer);
-                    send(fds[i].fd, response, strlen(response), 0);
-                }
-            }
-        }
-    }
-    
-    /* Clean up */
-    for (i = 0; i < MAX_CLIENTS; i++) {
-        if (client_fds[i] != -1) {
-            close(client_fds[i]);
-        }
-    }
-    close(server_fd);
-    
-    return 0;
+	while ((c = getopt(argc, argv, "nf:p:")) != -1) {
+		switch (c) {
+			case 'n':
+				newfile = true;
+				break;
+			case 'f':
+				filepath = optarg;
+				break;
+			case 'p':
+				portarg = optarg;
+				port = atoi(portarg);
+				if (port == 0) {
+					printf("Bad port: %s\n", portarg);
+				}
+				break;
+			case '?':
+				printf("Unknown option -%c\n", c);
+			default:
+				return -1;
+
+		}
+	}
+
+	if (filepath == NULL) {
+		printf("Filepath is a required argument\n");
+		print_usage(argv);
+
+		return 0;
+	}
+
+	if (port == 0) {
+		printf("Port not set\n");
+		print_usage(argv);
+		return 0;
+	}
+
+
+	if (newfile) {
+		dbfd = create_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to create database file\n");
+			return -1;
+		}
+
+		if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+			printf("Failed to create database header\n"); 
+			return -1;
+		}
+
+	} else {
+		dbfd = open_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to open database file\n");
+			return -1;
+		}
+
+		if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+			printf("Failed to validate database header\n");
+			return -1;
+		}
+	}
+
+	if (read_employees(dbfd, dbhdr, &employees) != STATUS_SUCCESS) {
+		printf("Failed to read employees");
+		return 0;
+	}
+
+	poll_loop(port, dbhdr, employees);
+
+	return 0;
 }
