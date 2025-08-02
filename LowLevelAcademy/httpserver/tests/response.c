3aa262ba76617e276bff6316a513860755dce984
holmen1 <holmen1@gmail.com>
2025-08-02 21:07:52 +0200

    begin response

---
+++ LowLevelAcademy/httpserver/tests/response.c
@@ -0,0 +1,22 @@
+#include "http.h"
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    http_response response;
+    init_http_response(&response);
+
+    add_http_header(&response, "Content-Type", "text/html");
+    add_http_header(&response, "Connection", "close");
+
+    printf("HTTP Response Headers:\n");
+    for (size_t i = 0; i < response.header_count; i++) {
+        printf("%s: %s\n", response.headers[i].key, response.headers[i].value);
+    }
+
+    free_http_response(&response);
+
+    return 0;
+}
+
+// $ gcc -I../include -o test_response response.c ../obj/http.o
