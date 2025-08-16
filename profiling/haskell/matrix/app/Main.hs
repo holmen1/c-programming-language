15734b147fea2f22b39162ca5fe46412c9c4f4fe
holmen1 <holmen1@gmail.com>
2025-08-16 12:52:43 +0200

    timing

--- profiling/haskell/matrix/app/Main.hs
+++ profiling/haskell/matrix/app/Main.hs
@@ -1,13 +1,35 @@
+
 module Main where
 
 import MyMatrix
+import Data.Time.Clock (getCurrentTime, diffUTCTime)
+import qualified Data.Vector.Unboxed as U
+
+-- Generate a matrix with m[i][j] = i + j
+genA :: Int -> Int -> Matrix Double
+genA rows cols = Matrix rows cols $ U.generate (rows * cols) $ \ix ->
+  let (i, j) = ix `divMod` cols in fromIntegral i + fromIntegral j
+
+-- Generate a matrix with m[i][j] = i * j
+genB :: Int -> Int -> Matrix Double
+genB rows cols = Matrix rows cols $ U.generate (rows * cols) $ \ix ->
+  let (i, j) = ix `divMod` cols in fromIntegral i * fromIntegral j
 
 main :: IO ()
 main = do
-  let m1 = fromLists [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]] :: Matrix Double
-  let m2 = fromLists [[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]] :: Matrix Double
-  print m1
-  print m2
-  print $ add m1 m1
-  print $ mul m1 m2
+  let aRows = 300
+      aCols = 1000
+      bRows = 1000
+      bCols = 400
+
+  putStrLn "Generating matrices..."
+  let a = genA aRows aCols
+      b = genB bRows bCols
+
+  putStrLn "Multiplying matrices..."
+  start <- getCurrentTime
+  let c = mul a b
+  end <- c `seq` getCurrentTime
+
+  putStrLn $ "Matrix multiplication took " ++ show (realToFrac (diffUTCTime end start) :: Double) ++ " seconds"
 
