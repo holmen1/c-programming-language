ddbf4bb32247d1ea9359e91e593b75bb361cd195
holmen1 <holmen1@gmail.com>
2025-08-16 12:26:04 +0200

    operators

--- profiling/haskell/matrix/app/Main.hs
+++ profiling/haskell/matrix/app/Main.hs
@@ -4,6 +4,10 @@ import MyMatrix
 
 main :: IO ()
 main = do
-  let m = fromLists [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]] :: Matrix Double
-  print m
+  let m1 = fromLists [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]] :: Matrix Double
+  let m2 = fromLists [[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]] :: Matrix Double
+  print m1
+  print m2
+  print $ add m1 m1
+  print $ mul m1 m2
 
--- profiling/haskell/matrix/src/MyMatrix.hs
+++ profiling/haskell/matrix/src/MyMatrix.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts #-}
+--{-# LANGUAGE FlexibleContexts #-}
 
 module MyMatrix where
 
@@ -17,3 +17,19 @@ fromLists rows =
   let r = length rows
       c = if null rows then 0 else length (head rows)
   in Matrix r c (U.fromList (concat rows))
+
+-- | Add two matrices elementwise
+add :: (Unbox a, Num a) => Matrix a -> Matrix a -> Matrix a
+add (Matrix r1 c1 v1) (Matrix r2 c2 v2)
+  | r1 /= r2 || c1 /= c2 = error "Matrix dimensions must match for addition"
+  | otherwise = Matrix r1 c1 (U.zipWith (+) v1 v2)
+
+
+-- | Matrix multiplication (dot product)
+mul :: (Unbox a, Num a) => Matrix a -> Matrix a -> Matrix a
+mul (Matrix r1 c1 v1) (Matrix r2 c2 v2)
+  | c1 /= r2 = error "Matrix dimensions do not match for multiplication"
+  | otherwise = Matrix r1 c2 $ U.generate (r1 * c2) $ \ix ->
+      let (i, j) = ix `divMod` c2
+      in sum [ (v1 U.! (i * c1 + k)) * (v2 U.! (k * c2 + j)) | k <- [0..c1-1] ]
+
