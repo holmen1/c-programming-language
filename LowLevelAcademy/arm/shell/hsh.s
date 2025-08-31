df483f566f55fdb0d556c2adf69360395f5bc487
holmen1 <holmen1@gmail.com>
2025-08-31 22:20:29 +0200

    execute command

--- LowLevelAcademy/arm/shell/hsh.s
+++ LowLevelAcademy/arm/shell/hsh.s
@@ -9,10 +9,8 @@ main_loop:
     bl print_prompt
     bl read_cmd
     bl check_exit
-    //bl fork
     bl create_cmd
-    bl print_result
-    //bl wait_for_child
+    bl exec_cmd
     b main_loop
 
 print_prompt:
@@ -33,21 +31,12 @@ read_cmd:
     ldr r0, =input_buf 
     pop {pc}
 
-fork:
-    push {lr}
-    mov r7, #2           // sys_fork
-    svc #0
-    pop {pc}
-
 create_cmd:
     push {lr}
-    // r0 = input buffer (e.g., "ls\n")
-
-    ldr r1, =buffer      // destination buffer for full path
+    ldr r1, =buffer         // destination buffer for full path
     ldr r2, =my_path        // source: "/usr/bin/"
     ldr r3, =path_len       // length of "/usr/bin/"
     mov r4, #0
-
 copy_path:
     cmp r4, r3
     beq append_cmd
@@ -55,38 +44,46 @@ copy_path:
     strb r5, [r1, r4]
     add r4, r4, #1
     b copy_path
-
 append_cmd:
     add r1, r1, r3          // r1 = buffer + path_len (append here)
     mov r2, r0              // r2 = original input buffer pointer
-
 copy_cmd:
     ldrb r5, [r2], #1
     cmp r5, #10             // newline?
     beq null_term
     strb r5, [r1], #1
     b copy_cmd
-
 null_term:
     mov r5, #0
     strb r5, [r1]           // null-terminate
     ldr r0, =buffer
     pop {pc}
 
-wait_for_child:
+exec_cmd:
     push {lr}
+    bl fork
+    cmp r0, #0
+    beq child_process
+    bl wait_for_child
+child_process:
+    push {lr}
+    mov r7, #11         // sys_execveq
+    ldr r0, =buffer
+    mov r1, #0        // argv
+    mov r2, #0          // envp
+    svc #0
+wait_for_child:
     mov r7, #0x72        // sys_wait4
     mov r0, #-1          // Wait for any child
     mov r1, #0           // No options
     mov r2, #0           // No status
     svc #0
-
     pop {pc}
 
-print_result:
+fork:
     push {lr}
-    mov r1, #12
-    bl printf
+    mov r7, #2              // sys_fork
+    svc #0
     pop {pc}
 
 printf:
@@ -127,7 +124,7 @@ my_welcome_msg:
 welcome_msg_len = . - my_welcome_msg
 
 my_path:
-    .asciz "/usr/bin/"
+    .ascii "/usr/bin/"
 path_len = . - my_path
 
 .equ buffer_len, 16    // 15 chars + null terminator
