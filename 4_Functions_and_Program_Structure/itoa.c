62fc8c51fc327a78ca32e3dab99b5a93b41e9d58
holmen1 <holmen1@gmail.com>
2026-06-28 13:59:54 +0200

    overkill

--- 4_Functions_and_Program_Structure/itoa.c
+++ 4_Functions_and_Program_Structure/itoa.c
@@ -2,25 +2,26 @@
 
 #define MAXLEN 6
 
-static int i = 0;
+static int itoa_rec(int n, char s[], size_t size, int *i) {
+  if (n / 10)
+    itoa_rec(n / 10, s, size, i);
+  if (*i + 1 < size) {
+    s[(*i)++] = '0' + n % 10;
+    return 1;
+  } else
+    return 0;
+}
 
-void itoa_helper(int n, char s[]) {
+int itoa(int n, char s[], size_t size) {
+  int i = 0;
+  int is_success;
   if (n < 0) {
     s[i++] = '-';
     n = -n;
   }
-  if (n / 10)
-    itoa_helper(n / 10, s);
-  if (i < MAXLEN - 1)
-    s[i++] = n % 10 + '0';
-  else
-    fprintf(stderr, "%d to long, result is truncated\n", n);
-}
-
-void itoa(int n, char s[]) {
-  itoa_helper(n, s);
+  is_success = itoa_rec(n, s, size, &i);
   s[i] = '\0';
-  i = 0;
+  return is_success;
 }
 
 int main(int argc, char *argv[]) {
@@ -28,10 +29,9 @@ int main(int argc, char *argv[]) {
   int examples[] = {123, -1234, 12345, 123456, -12345};
   int k = 0;
   for (; k < 5; k++) {
-    printf("itoa(%d, s)\n", examples[k]);
-    itoa(examples[k], res);
-    printf("s = %s\n", res);
+    if (!itoa(examples[k], res, sizeof res))
+      fprintf(stderr, "%d to long, result is truncated\n", examples[k]);
+    printf("itoa(%d) -> %s\n", examples[k], res);
   }
-
   return 0;
 }
