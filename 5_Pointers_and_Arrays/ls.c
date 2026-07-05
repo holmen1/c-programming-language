09803d48856cecf5a56bc41e08e887e43ca977cb
holmen1 <holmen1@gmail.com>
2026-07-05 19:53:50 +0200

    bonus: ls (simplified)

--- 5_Pointers_and_Arrays/ls.c
+++ 5_Pointers_and_Arrays/ls.c
@@ -3,48 +3,51 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void usage(char *program) {
+static void usage(char *program) {
   fprintf(stderr, "Usage: %s [-a] [path]\n", program);
 }
 
-int main(int argc, char *argv[]) {
-  int opt;
-  int all = 0;
-
-  while ((opt = getopt(argc, argv, "a")) != -1) {
-    switch (opt) {
-    case 'a':
-      all = 1;
-      break;
-    default:
-      usage(argv[0]);
-      return 1;
-    }
-  }
-
-  char *name;
-  if (argc == optind)
-    name = ".";
-  else if (argc == optind + 1)
-    name = argv[optind];
-  else {
-    usage(argv[0]);
-    return 1;
-  }
-
+static void _ls(char *name, int list_all) {
   DIR *dir = opendir(name);
-  if (!dir) {
+  if (!dir)
     fprintf(stderr, "%s: Not a directory\n", name);
-    return 1;
-  }
 
   struct dirent *entry;
   while ((entry = readdir(dir)) != NULL) {
-    if (*(*entry).d_name == '.' && !all)
+    if (*(*entry).d_name == '.' && !list_all)
       continue;
     printf("%s\n", (*entry).d_name);
   }
 
   closedir(dir);
+}
+
+int main(int argc, char *argv[]) {
+  int opt;
+
+  opt = getopt(argc, argv, "a");
+  if (opt == 'a') {
+    if (argc == 2)
+      _ls(".", 1);
+    if (argc == 3)
+      _ls(argv[optind], 1);
+    else {
+      usage(argv[0]);
+      return 1;
+    }
+  } else if (opt == -1) {
+    if (argc == 1)
+      _ls(".", 0);
+    if (argc == 2)
+      _ls(argv[optind], 0);
+    else {
+      usage(argv[0]);
+      return 1;
+    }
+  } else {
+    usage(argv[0]);
+    return 1;
+  }
+
   return 0;
 }
