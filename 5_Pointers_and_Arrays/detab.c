3caca56edf4f8e2b5f5ddf5c806dd9adbec68084
holmen1 <holmen1@gmail.com>
2026-08-06 09:35:39 +0200

    cleanup

--- 5_Pointers_and_Arrays/detab.c
+++ 5_Pointers_and_Arrays/detab.c
@@ -4,47 +4,40 @@
 #define TABN 8
 #define MAXLINELEN 100
 
-void entabln(char *spacedln, const char *ln, int tablen);
-char *entab(char *spacedln, int steps);
+static void detab_line(char *dst, const char *src, int tab_width) {
+  int c, nspaces;
+  int col = 0;
+  while ((c = *src++) != '\n') {
+    if (c == '\t') {
+      nspaces = tab_width - (col % tab_width);
+      while (nspaces--) {
+        *dst++ = ' ';
+        col++;
+      }
+    } else {
+      *dst++ = c;
+      col++;
+    }
+  }
+  *dst++ = '\n';
+  *dst = '\0';
+}
 
 int main(int argc, char *argv[]) {
   char *buffer = NULL;
   size_t bufsize = 0; // Size of the buffer
-  ssize_t c;          // Number of characters read
-  char spaced[MAXLINELEN];
+  ssize_t nread;          // Number of characters read
+  char out[MAXLINELEN];
 
-  while ((c = getline(&buffer, &bufsize, stdin)) != -1) {
-    if (c > MAXLINELEN) {
+  while ((nread = getline(&buffer, &bufsize, stdin)) != -1) {
+    if (nread > MAXLINELEN) {
       free(buffer);
       return 1;
     }
 
-    entabln(spaced, buffer, TABN);
-    printf("%s", spaced);
+    detab_line(out, buffer, TABN);
+    fputs(out, stdout);
   }
   free(buffer);
   return 0;
-}
-
-void entabln(char *spacedln, const char *ln, int tablen) {
-  int c, steps;
-  int col = 0;
-  while ((c = *ln++) != '\n') {
-    if (c == '\t') {
-      steps = tablen - (col % tablen);
-      spacedln = entab(spacedln, steps);
-      col = 0;
-    } else {
-      *spacedln++ = c;
-      col++;
-    }
-  }
-  *spacedln++ = '\n';
-  *spacedln = '\0';
-}
-
-char *entab(char *spacedln, int steps) {
-  while (steps--)
-    *spacedln++ = ' ';
-  return spacedln;
 }
