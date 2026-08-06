195cf377ec3130951266de37a606a3f4474e4d8c
holmen1 <holmen1@gmail.com>
2026-08-06 08:53:08 +0200

    Exercise 5-11: detab using getline

--- 5_Pointers_and_Arrays/detab.c
+++ 5_Pointers_and_Arrays/detab.c
@@ -1,37 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TABN 8
+#define MAXLINELEN 100
 
-void print_spaces(int n);
+void entabln(char *spacedln, const char *ln, int tablen);
+char *entab(char *spacedln, int steps);
 
 int main(int argc, char *argv[]) {
-  int tabn;
-  tabn = TABN;
+  char *buffer = NULL;
+  size_t bufsize = 0; // Size of the buffer
+  ssize_t c;          // Number of characters read
+  char spaced[MAXLINELEN];
 
-  int c;
-  int col = 0;
-  int step;
-  while ((c = getchar()) != EOF) {
-    switch (c) {
-    case '\n':
-      printf("%c", c);
-      col = 0;
-      break;
-    case '\t':
-      step = tabn - (col % tabn);
-      print_spaces(step);
-      col += step;
-      break;
-    default:
-      printf("%c", c);
-      col++;
-      break;
+  while ((c = getline(&buffer, &bufsize, stdin)) != -1) {
+    if (c > MAXLINELEN) {
+      free(buffer);
+      return 1;
     }
+
+    entabln(spaced, buffer, TABN);
+    printf("%s", spaced);
   }
+  free(buffer);
   return 0;
 }
 
-void print_spaces(int n) {
-  while (n--)
-    printf(" ");
+void entabln(char *spacedln, const char *ln, int tablen) {
+  int c, steps;
+  int col = 0;
+  while ((c = *ln++) != '\n') {
+    if (c == '\t') {
+      steps = tablen - (col % tablen);
+      spacedln = entab(spacedln, steps);
+      col = 0;
+    } else {
+      *spacedln++ = c;
+      col++;
+    }
+  }
+  *spacedln++ = '\n';
+  *spacedln = '\0';
+}
+
+char *entab(char *spacedln, int steps) {
+  while (steps--)
+    *spacedln++ = ' ';
+  return spacedln;
 }
