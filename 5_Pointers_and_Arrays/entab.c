1292182f36177c5df90f4f9d117657b171e8341b
holmen1 <holmen1@gmail.com>
2026-08-06 11:03:23 +0200

    cleanup

--- 5_Pointers_and_Arrays/entab.c
+++ 5_Pointers_and_Arrays/entab.c
@@ -1,39 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TABN 8
+#define MAXLINELEN 100
 
-void print_spaces(int n);
-
-int main(int argc, char *argv[]) {
-  int tabn;
-  tabn = TABN;
-
+static void entab_line(char *dst, const char *src, int tab_width) {
   int c;
-  int col = 0;
-  int spaces = 0;
-  while ((c = getchar()) != EOF) {
-    col++;
+  int nspaces = 0;
+  int col = 1;
+  while ((c = *src++) != '\n') {
     if (c == ' ') {
-      spaces++;
-      if (col % tabn == 0) {
-        printf("\t");
-        spaces = 0;
+      nspaces++;
+      if (col % tab_width == 0) {
+        *dst++ = '\t';
+        nspaces = 0;
       }
+      col++;
       continue;
     }
-    if (spaces) {
-      print_spaces(spaces);
-      spaces = 0;
+    while (nspaces) {
+      *dst++ = ' ';
+      nspaces--;
     }
-    printf("%c", c);
-    if (c == '\n')
-      col = 0;
+    *dst++ = c;
+    col++;
   }
-
-  return 0;
+  *dst++ = '\n';
+  *dst = '\0';
 }
 
-void print_spaces(int n) {
-  while (n--)
-    printf(" ");
+int main(int argc, char *argv[]) {
+  char *buffer = NULL;
+  size_t bufsize = 0; // Size of the buffer
+  ssize_t nread;      // Number of characters read
+  char out[MAXLINELEN];
+
+  while ((nread = getline(&buffer, &bufsize, stdin)) != -1) {
+    if (nread > MAXLINELEN) {
+      free(buffer);
+      return 1;
+    }
+
+    entab_line(out, buffer, TABN);
+    fputs(out, stdout);
+  }
+  free(buffer);
+  return 0;
 }
